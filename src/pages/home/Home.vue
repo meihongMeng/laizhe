493d277af0699385355961a857286ef6ec7f9f37
meihongMeng <15095206101@163.com>
2018-07-02 17:44:49 +0800

    change

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,17 +2,20 @@
   <div>
     <index-header></index-header>
     <index-swiper></index-swiper>
+    <index-iconSwiper></index-iconSwiper>
   </div>
 </template>
 <script>
 
 import Header from "./components/Header"
 import Swiper from "./components/Swiper"
+import IconSwiper from "./components/IconSwiper"
 
 export default {
   components: {
     "index-header": Header,
-    "index-swiper": Swiper
+    "index-swiper": Swiper,
+    "index-iconSwiper": IconSwiper
   }
 }
 </script>
